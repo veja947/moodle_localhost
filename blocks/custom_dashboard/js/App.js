ae0c786ad732cd3c4de450804f2077a8a7b80eba
veja947 <veja947@gmail.com>
2021-08-06 10:49:09 -0400

    [block/custom_dashboard] 1. react continue, remove material table

--- blocks/custom_dashboard/js/App.js
+++ blocks/custom_dashboard/js/App.js
@@ -21,39 +21,8 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {Route, Switch} from "react-router";
 import Select from 'react-select';
-import MaterialTable from 'material-table';
 import ProgressBar from './components/ProgressBar';
 
-const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
-
-// const column =[
-//     { title: 'Campaigns in progress', field: 'campaigns' },
-//     { title: 'Total students', field: 'total_students', type: 'numeric' },
-//     {
-//         title: 'Progress',
-//         field: 'progress',
-//         render: rowData => <ProgressBar readings={ rowData.progress } />
-//     },
-//     { title: 'Completion rate', field: 'completion_rate' }
-// ];
 let table_columns = [
     {
         title: 'Campaigns in progress',
@@ -125,14 +94,6 @@ let table_data = [
     },
 ];
 
-// let data = [
-//     { campaigns: "program1", total_students: 123, progress: readings_data, completion_rate: "54%" },
-//     { campaigns: "program2", total_students: 456, progress: readings_data, completion_rate: "12%" },
-//     { campaigns: "program3", total_students: 321, progress: readings_data, completion_rate: "44%" },
-//     { campaigns: "program4", total_students: 436, progress: readings_data, completion_rate: "78%" },
-//     { campaigns: "program5", total_students: 856, progress: readings_data, completion_rate: "99%" },
-// ];
-
 const options = [
     { value: 'program111', label: 'program111' },
     { value: 'program222', label: 'program222' },
@@ -165,14 +126,6 @@ class App extends Component {
                         columns={table_columns}
                         dataSource={table_data}
                     />
-                    {/*<MaterialTable*/}
-                    {/*    icons={tableIcons}*/}
-                    {/*    columns={column}*/}
-                    {/*    data={data}*/}
-                    {/*    options={{*/}
-                    {/*        search: true*/}
-                    {/*    }}*/}
-                    {/*/>*/}
                     <Switch>
                         <Route path="/">
                             <h5>new Dashboard</h5>
